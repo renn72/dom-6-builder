8a7cab4b3245406ba4921868cb0610756a5112e1
renn72 <simplesam8856@gmail.com>
2024-02-20 13:45:50 +1100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/navbar'
+
+function App() {
+  return (
+    <main className='flex flex-col items-center h-screen'>
+        <Navbar />
+      <div className='flex flex-col items-center justify-center gap-y-4 w-full h-full'>
+      </div>
+    </main>
+  )
+}
+
+export default App
